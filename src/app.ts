7aa96841ec681c6f35c9961c13fa2d645b62f510
EpicAryan <adaryan0811@gmail.com>
2024-10-27 23:21:38 +0530

    Refactor app.ts to update route path for API welcome message

--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,7 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
-app.use("/", (req, res) => {
+app.use("/api/hello", (req, res) => {
     res.send("Welcome to the API");
 });
 app.use("/api/v1/users", userRouter);
