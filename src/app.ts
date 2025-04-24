1e5a8442bf94df6d2a4a880d3f621fb98747727b
EpicAryan <adaryan0811@gmail.com>
2025-04-25 01:28:08 +0530

    fix: type error for courseName

--- src/app.ts
+++ src/app.ts
@@ -14,7 +14,6 @@ app.use(cors({
     credentials: true,
 }));
 
-app.use("/api/v1", paymentRouter);
 app.use((req, res, next) => {
     if(req.originalUrl === '/api/v1/webhook'){
         next()
@@ -31,5 +30,6 @@ app.get("/api/hello", (req:Request, res:Response) => {
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/modules", moduleRouter);
 app.use("/api/v1/profiles", userProfileRouter);
+app.use("/api/v1", paymentRouter);
 
 export default app;
