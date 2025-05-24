8869830437cea32909b0c377592a18c0acc4a54d
EpicAryan <adaryan0811@gmail.com>
2025-05-24 22:01:00 +0530

    refactor: changePasswordSchema

--- src/schemas/changePasswordSchema.ts
+++ src/schemas/changePasswordSchema.ts
@@ -1,9 +1,13 @@
 import { z } from 'zod';
 
-const passwordSchema = z.string().min(8, "Password must be at least 8 characters long");
+const passwordSchema = z.string().min(8, "Password must be at least 8 characters long")
+                        .regex(
+                          /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+                          'Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character'
+                        );
 
 export const changePasswordSchema = z.object({
-  oldPassword: passwordSchema,
+  oldPassword:z.string().min(8, "Password must be at least 8 characters long"),
   newPassword: passwordSchema
 });
 
